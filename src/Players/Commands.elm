6c28202e17d0295ac5198ddd765fa5e8dcb046e0
rubencosta <rmc@nubolab.org>
2016-08-11 20:29:42 +0200

    edit level

--- src/Players/Commands.elm
+++ src/Players/Commands.elm
@@ -2,6 +2,7 @@ module Players.Commands exposing (..)
 
 import Http
 import Json.Decode as Decode exposing ((:=))
+import Json.Encode as Encode
 import Task
 import Players.Models exposing (PlayerId, Player)
 import Players.Messages exposing (..)
@@ -9,7 +10,8 @@ import Players.Messages exposing (..)
 
 fetchAll : Cmd Msg
 fetchAll =
-    Http.get collectionDecoder fetchAllUrl |> Task.perform FetchAllFail FetchAllDone
+    Http.get collectionDecoder fetchAllUrl
+        |> Task.perform FetchAllFail FetchAllDone
 
 
 fetchAllUrl : String
@@ -28,3 +30,46 @@ memberDecoder =
         ("id" := Decode.int)
         ("name" := Decode.string)
         ("level" := Decode.int)
+
+
+saveUrl : PlayerId -> String
+saveUrl playerId =
+    "http://localhost:4000/players/" ++ (toString playerId)
+
+
+saveTask : Player -> Task.Task Http.Error Player
+saveTask player =
+    let
+        body =
+            memberEncoder player
+                |> Encode.encode 0
+                |> Http.string
+
+        config =
+            { verb = "PATCH"
+            , headers = [ ( "Content-Type", "application/json" ) ]
+            , url = saveUrl player.id
+            , body = body
+            }
+    in
+        Http.send Http.defaultSettings config
+            |> Http.fromJson memberDecoder
+
+
+save : Player -> Cmd Msg
+save player =
+    saveTask player
+        |> Task.perform SaveFail SaveSuccess
+
+
+memberEncoder : Player -> Encode.Value
+memberEncoder player =
+    let
+        list =
+            [ ( "id", Encode.int player.id )
+            , ( "name", Encode.string player.name )
+            , ( "level", Encode.int player.level )
+            ]
+    in
+        list
+            |> Encode.object
--- src/Players/Update.elm
+++ src/Players/Update.elm
@@ -2,7 +2,8 @@ module Players.Update exposing (..)
 
 import Navigation
 import Players.Messages exposing (Msg(..))
-import Players.Models exposing (Player)
+import Players.Models exposing (Player, PlayerId)
+import Players.Commands exposing (save)
 
 
 update : Msg -> List Player -> ( List Player, Cmd Msg )
@@ -19,3 +20,37 @@ update msg players =
 
         ShowPlayer id ->
             ( players, Navigation.newUrl ("#players/" ++ (toString id)) )
+
+        ChangeLevel id howMutch ->
+            ( players, changeLevelCommands id howMutch players |> Cmd.batch )
+
+        SaveSuccess player ->
+            updatePlayer player players
+
+        SaveFail string ->
+            ( players, Cmd.none )
+
+
+changeLevelCommands : PlayerId -> Int -> List Player -> List (Cmd Msg)
+changeLevelCommands id howMutch players =
+    let
+        cmdForPlayer player =
+            if player.id == id then
+                save
+                    { player | level = player.level + howMutch }
+            else
+                Cmd.none
+    in
+        List.map cmdForPlayer players
+
+
+updatePlayer : Player -> List Player -> ( List Player, Cmd Msg )
+updatePlayer updatedPlayer players =
+    let
+        updatePlayer player =
+            if player.id == updatedPlayer.id then
+                updatedPlayer
+            else
+                player
+    in
+        ( List.map updatePlayer players, Cmd.none )
